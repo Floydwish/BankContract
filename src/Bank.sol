9684e954da4df123b1b36efa0a88d41beb1ba1cb
Floydwish <my0127912@hotmail.com>
2025-09-26 14:44:35 +0800

    更新:防重入的说明

--- src/Bank.sol
+++ src/Bank.sol
@@ -61,6 +61,47 @@ contract myBank{
     }
 
     // 取款函数，用于从合约地址提取资金
+   /*
+    1. 防重入的必要性
+      a. 当前版本的 bank 合约从技术上不用防重入（只有 owner 可以调用，且有少量 gas 开销）
+      b. 但是遵循最佳安全实践，任何涉及外部 ETH 发送的函数都应该有 防重入 保护。（因为：1.实际开发中后续可能添加功能，导致暴露在重入攻击下；2.代码审查友好；3.成本低）
+
+    2. 重入攻击的原理
+    // ❌ 有漏洞
+    function withdraw() public {
+        // 1. 检查余额
+        require(balance[msg.sender] > 0);  
+
+        // 2. 发送资金
+        // 此时，恶意合约可以在 fallback 中再次调用 withdraw, 导致重入（第1步的余额还未更新）
+        (bool success,) = msg.sender.call{value: balance[msg.sender]}("");
+        require(success);
+
+        // 3. 更新余额
+        balance[msg.sender] = 0;
+    }
+
+    3. 防重入攻击的原理
+    // ✅ 修复后
+    // 机制：Check-Effects-Interactions 模式
+    function withdraw() public {
+        // 1. 检查余额
+        uint256 amount = balance[msg.sender];
+
+        // 2. 更新余额
+        balance[msg.sender] = 0;
+
+        // 3. 发送资金
+        // 此时，恶意合约无法在 fallback 中再次调用 withdraw, 因为第2步的余额已经更新(如果重入，将通不过第 1 步的检查)
+        (bool success,) = msg.sender.call{value: amount}("");
+        require(success);
+    }
+
+    4. 防重入的实际使用（同时使用 a, b 两种方式)
+     a. 防护方法1：先更新状态，再转账 (机制：Check-Effects-Interactions 模式)
+     b. 防护方法2：使用 ReentrancyGuard 修饰器（ OpenZeppelin 库中有）
+
+    */  
     function withdraw(uint256 _amount) public onlyOwner reentrantGuard{
         // 1. 检查合约总存款金额是否足够
         require(address(this).balance >= _amount, "Insufficient balance");
